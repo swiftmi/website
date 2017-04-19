2b6a3a031aa4ebbf616d5fb611ab8aa665c0986e
feiin <feiin@qq.com>
2017-04-19 11:06:19 +0800

    Added mysql-provider

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,12 +1,15 @@
 import Vapor
+import VaporMySQL
 
 let drop = Droplet()
+try drop.addProvider(VaporMySQL.Provider.self)
 
 drop.get { req in
     return try drop.view.make("welcome", [
     	"message": drop.localization[req.lang, "welcome", "title"]
     ])
 }
+
 
 drop.resource("posts", PostController())
 
